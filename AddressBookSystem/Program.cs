b3dbf4b9b1309f8333a361eba89c76f6bc8b2e3d
Prasad <banprasad21@gmail.com>
2022-09-18 23:37:05 +0530

    [Raghav] Add: UC1 ability to create a contact in address book

---
+++ AddressBookSystem/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    // Interface created for abstraction and easy access for user
+  
+    public interface IAddressBook
+    {
+        void AddContact(string firstName, string lastName, string address, string city, string state, string zip, string phonenumber, string email);
+        void DisplayPhoneNumber(string Name);
+
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book Program");
+
+            AddressBook addressBook = new AddressBook();
+
+            // Adding contact into address book
+            addressBook.AddContact("Prasad", "Ban", "Street NO: 39", "NewYork", "America", "123456", "1234567891", "sample@example.com");
+
+            // Searching phone Number by contact name
+            addressBook.DisplayPhoneNumber("Prasad");
+
+        }
+    }
+}
